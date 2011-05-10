ce30a5a2eb08609676d854963855f763e1b4ff46
stickboy <pete.sowerbutts@gmail.com>
2011-05-09 18:22:02 -0700

    Added a messenger object to the Runner

--- lib/town/runner.rb
+++ lib/town/runner.rb
@@ -2,14 +2,21 @@ require 'town/clock'
 
 module Town
   class Runner
-    def start 
-      clock = Clock.new
 
-      while true do
-        clock.tick
-        print "#{clock.time}\n"
-        sleep 1
-      end
+    def initialize(messenger)
+      @messenger = messenger
+      @clock = Clock.new
+    end 
+ 
+    def start 
+      @messenger.puts "Welcome to the Town!"
+      
+#
+#      while true do
+#        clock.tick
+#        print "#{clock.time}\n"
+#        sleep 1
+#      end
     end
   end
 end
--- spec/town/runner_spec.rb
+++ spec/town/runner_spec.rb
@@ -1,9 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require 'town/clock'
-require 'town/runner'
 
-describe Town::Runner do
+module Town
+  describe Runner do
 
-  
+    context "Starting up" do
 
+      before(:each) do
+        @messenger = mock("messenger").as_null_object
+        @runner = Runner.new(@messenger)
+      end
+
+      it "should send a welcome message" do
+        @messenger.should_receive(:puts).with("Welcome to the Town!")
+        @runner.start
+      end
+      
+    end
+
+  end
 end
