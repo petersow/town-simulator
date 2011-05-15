dfc37ce8068d8e924d691ffcc6cddb1b76f791c5
Peter Sowerbutts <pete.sowerbutts@gmail.com>
2011-05-15 06:22:23 -0700

    Added to module Town

--- lib/town/location.rb
+++ lib/town/location.rb
@@ -1,11 +1,11 @@
-class Location
+module Town
+  class Location
+    attr_accessor :x, :y, :z
 
-  attr_accessor :x, :y, :z
-
-  def initialize(options = {})
-    @x = options[:x] ||= 0
-    @y = options[:y] ||= 0
-    @z = options[:z] ||= 0
+    def initialize(options = {})
+      @x = options[:x] ||= 0
+      @y = options[:y] ||= 0
+      @z = options[:z] ||= 0
+    end
   end
-
 end
