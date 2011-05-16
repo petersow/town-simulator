ecef62b9479af52559a2c7dc3f0fe1ec8e9ceb5d
Pete Sowerbutts <pete.sowerbutts@gmail.com>
2011-05-16 10:48:29 +0100

    Added a name to Action

--- lib/town/action.rb
+++ lib/town/action.rb
@@ -2,10 +2,12 @@ require 'securerandom'
 
 module Town
   class Action
+    attr_accessor :name
     attr_reader :a_id
 
-    def initialize
+    def initialize(options = {})
       @a_id = SecureRandom.uuid
+      @name = options[:name] ||= ""
     end
   end
 end
--- spec/town/action_spec.rb
+++ spec/town/action_spec.rb
@@ -10,5 +10,34 @@ module Town
       @action.a_id.should_not be_nil
     end
 
+    it "has a unique a_id" do
+      actions = []
+      1000.times do
+        actions << Action.new
+      end
+
+      actions.each do |action|
+        action.a_id.should_not eql @action.a_id
+      end
+    end
+
+    it "should not let a_id be set" do
+      lambda{@action.a_id = 1}.should raise_error 
+    end
+
+    it "should have a name" do
+      @action.name.should_not be_nil
+    end
+
+    it "should let name be changed" do
+      @action.name = "Idle"
+      @action.name.should eql "Idle"
+    end
+
+    it "should let name be set at creation" do
+      action = Action.new(:name => "Running") 
+      action.name.should eql "Running"
+    end
+
   end
 end
