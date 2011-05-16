3338b8e10d5ae416065bae33cc8cbd3477103953
Peter Sowerbutts <pete.sowerbutts@gmail.com>
2011-05-16 13:56:34 -0700

    Changed Thing to use IdleAction

--- lib/town/thing.rb
+++ lib/town/thing.rb
@@ -8,7 +8,6 @@ module Town
     def initialize(options = {})
       @t_id = SecureRandom.uuid
       @location = options[:location] ||= Location.new
-      # Need to change to a lookup so they all use the same object
       @current_action = IdleAction.new
     end
 
