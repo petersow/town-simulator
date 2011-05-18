94f1ff711ebae7f7f15c9394e49c86f448e37a9f
Peter Sowerbutts <pete.sowerbutts@gmail.com>
2011-05-18 15:34:05 -0700

    Specced out sleeping times

--- lib/town/action_generator.rb
+++ lib/town/action_generator.rb
@@ -12,10 +12,22 @@ module Town
         raise ArgumentError
       end
       if thing.is_a? Person
-        if @time.hour >= thing.bedtime_hour
+        if thing.current_action.is_a? SleepAction
+          if @time.hour >= thing.wake_up_hour
+            return WakeAction.new
+          end
           return SleepAction.new
+        elsif thing.bedtime_hour > thing.wake_up_hour
+          if @time.hour >= thing.bedtime_hour or @time.hour < thing.wake_up_hour
+            return SleepAction.new
+          end
+          return IdleAction.new
+        elsif thing.bedtime_hour < thing.wake_up_hour
+          if @time.hour >= thing.bedtime_hour and @time.hour < thing.wake_up_hour
+            return SleepAction.new
+          end
+          return IdleAction.new
         end
-        return IdleAction.new
       end
     end
 
--- spec/town/action_generator_spec.rb
+++ spec/town/action_generator_spec.rb
@@ -7,6 +7,9 @@ module Town
       @person = Person.new(:first_name => "Mr",
                            :family_name => "Test")
       @action_generator = ActionGenerator.new
+
+      @midday = Time.new(:hour => 12)
+      @action_generator.time = @midday
     end
  
     it "should hold a copy of the time" do
@@ -34,24 +37,96 @@ module Town
     end
   
     context "bed time" do
-  
       before(:each) do
-        @person.bedtime_hour = 4
-        @three = Time.new(:hour => 3)
-        @four = Time.new(:hour => 4)
-        @action_generator.time = @three
+        @person.bedtime_hour = 23
+        @twentytwo = Time.new(:hour => 22)
+        @twentythree = Time.new(:hour => 23)
+        @midnight = Time.new(:hour => 0)
+        @action_generator.time = @twentytwo
       end
 
-      it "should have next_action return IdleAction if it is before." do
+      it "should have next_action return IdleAction if it is before" do
         @action_generator.next_action(@person).should be_is_a IdleAction
       end
 
-      it "should have next_action return SleepAction if it is after." do
-        @action_generator.time = @four
+      it "should have next_action return SleepAction if it is after" do
+        @action_generator.time = @twentythree
+        @action_generator.next_action(@person).should be_is_a SleepAction
+      end
+
+      it "should have next_action return SleepAction if it is after and the next day" do
+        @action_generator.time = @midnight
         @action_generator.next_action(@person).should be_is_a SleepAction
       end
 
     end
+   
+    context "wake time" do
+      before(:each) do
+        @person.wake_up_hour = 7
+        @person.current_action = SleepAction.new
+        @six = Time.new(:hour => 6)
+        @seven = Time.new(:hour => 7)
+        @action_generator.time = @six
+      end
 
+      it "should have next_action return SleepAction if it is already Sleeping and is before" do
+        @action_generator.next_action(@person).should be_is_a SleepAction
+      end
+
+      it "should have next_action return WakeAction if it is already Sleeping and is after" do
+        @action_generator.time = @seven
+        @action_generator.next_action(@person).should be_is_a WakeAction
+      end
+    end
+
+    context "night sleeper" do
+
+      before(:each) do
+        @person.bedtime_hour = 1
+        @person.wake_up_hour = 10
+      end 
+
+      context "bed time" do
+        before(:each) do
+          @twentythree = Time.new(:hour => 0)
+          @midnight = Time.new(:hour => 0)
+          @one = Time.new(:hour => 1)
+        end
+
+        it "should have next_action return IdleAction if it is the day before" do
+          @action_generator.time = @twentythree
+          @action_generator.next_action(@person).should be_is_a IdleAction
+        end
+
+        it "should have next_action return IdleAction if it is before" do
+          @action_generator.time = @midnight
+          @action_generator.next_action(@person).should be_is_a IdleAction
+        end
+
+        it "should have next_action return SleepAction if it is after" do
+          @action_generator.time = @one
+          @action_generator.next_action(@person).should be_is_a SleepAction
+        end
+      end
+     
+      context "wake time" do
+        before(:each) do
+          @person.current_action = SleepAction.new
+          @nine = Time.new(:hour => 9)
+          @ten = Time.new(:hour => 10)
+        end
+
+        it "should have next_action return SleepAction if it is already Sleeping and is before" do
+          @action_generator.time = @nine
+          @action_generator.next_action(@person).should be_is_a SleepAction
+        end
+  
+        it "should have next_action return WakeAction if it is already Sleeping and is after" do
+          @action_generator.time = @ten
+          @action_generator.next_action(@person).should be_is_a WakeAction
+        end
+      end
+    end
   end
 end
