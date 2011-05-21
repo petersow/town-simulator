e2943df0af66ec1f2fedcdc764e01f27582b6ad3
Peter Sowerbutts <pete.sowerbutts@gmail.com>
2011-05-21 12:49:45 -0700

    fleshed out EatAction

--- lib/town/eat_action.rb
+++ lib/town/eat_action.rb
@@ -1,20 +1,25 @@
 module Town
   class EatAction < Action
 
-    attr_accessor :meal_name
+    attr_accessor :meal_name, :time_remaining
 
     def initialize(options = {})
       super(:name => "Eat",
-            :type => "passive")
+            :type => "active")
       @meal_name = options[:meal_name] ||= "Snack"
+      @time_remaining = 20
     end
 
     def to_s
       "Eating #{@meal_name}"
     end
 
+    def step
+      @time_remaining -= 1
+    end
+
     def is_finished?
-      true
+      return @time_remaining <= 0
     end
 
   end
--- spec/town/eat_action_spec.rb
+++ spec/town/eat_action_spec.rb
@@ -20,12 +20,8 @@ module Town
       @eat_action.to_s.should eql "Eating Brunch"
     end
 
-    it "should have a type of passive" do
-      @eat_action.type.should eql "passive"
-    end
-
-    it "should return true for is_finished?" do
-      @eat_action.should be_is_finished
+    it "should have a type of active" do
+      @eat_action.type.should eql "active"
     end
 
     it "should have a meal name" do
@@ -45,6 +41,31 @@ module Town
       dinner = EatAction.new(:meal_name => "Dinner")
       dinner.meal_name.should eql "Dinner"
     end
+   
+    it "should have a time_remaining" do
+      @eat_action.time_remaining.should_not be_nil
+    end
 
+    it "should default time_remaining to 20" do
+      @eat_action.time_remaining.should eql 20
+    end
+
+    it "should return true for is_finished? if time_remaining is 0 or below" do
+      @eat_action.time_remaining = 0
+      @eat_action.should be_is_finished
+      @eat_action.time_remaining = -1
+      @eat_action.should be_is_finished
+    end
+
+    it "should return false for is_finished? if time_remaining is above 0" do
+      @eat_action.time_remaining = 1
+      @eat_action.should_not be_is_finished
+    end
+
+    it "should decrement time_remaining by 1 for step" do
+      @eat_action.time_remaining = 4
+      @eat_action.step
+      @eat_action.time_remaining.should eql 3
+    end
   end
 end
